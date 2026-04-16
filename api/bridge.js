dca70d4e2f8efb147adb6ffa716bb20db476f74c
Dʀ.Kei1 <80795572+DrKei1@users.noreply.github.com>
2026-04-17 02:35:28 +0330

    Update bridge.js

--- api/bridge.js
+++ api/bridge.js
@@ -5,28 +5,24 @@ export const config = {
 };
 
 export default async function handler(req, res) {
-  try {
-    const chunks = [];
+  const session = req.headers['x-session-id'] || "default";
 
-    for await (const chunk of req) {
-      chunks.push(chunk);
-    }
-
-    const body = Buffer.concat(chunks);
-
-    const response = await fetch("http://77.68.22.95:8080", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/octet-stream",
-      },
-      body: body,
-    });
-
-    const arrayBuffer = await response.arrayBuffer();
-
-    res.status(200).send(Buffer.from(arrayBuffer));
-  } catch (e) {
-    console.error(e);
-    res.status(500).send("error");
+  const chunks = [];
+  for await (const chunk of req) {
+    chunks.push(chunk);
   }
+
+  const body = Buffer.concat(chunks);
+
+  const response = await fetch("http://77.68.22.95:8080", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/octet-stream",
+      "x-session-id": session
+    },
+    body: body,
+  });
+
+  const data = await response.arrayBuffer();
+  res.status(200).send(Buffer.from(data));
 }
